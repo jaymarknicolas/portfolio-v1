fff932e98e4920d77703006c6cf891c45e51e2d3
jaymarknicolas <jmnicolas4me@gmail.com>
2025-05-04 14:52:14 +0800

    update resume

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -80,7 +80,7 @@ const techStack: TechItem[] = [
 
 export function TechStack() {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 gap-2">
+    <div className="grid grid-cols-2 lg:grid-cols-3 gap-2">
       {techStack.map((tech, index) => (
         <Badge
           key={index}
