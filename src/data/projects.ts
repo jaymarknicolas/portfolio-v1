0dff6444d3e80d610a4d13620d4df97e47648973
jaymarknicolas <jmnicolas4me@gmail.com>
2025-05-11 00:45:41 +0800

    update projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,7 +3,7 @@ export const projects = [
     name: "Happy Feet",
     description:
       "An Inventory & POS System for a retail business specializing in shoes and apparel, developed using Next.js, Prisma, PostgreSQL, and ShadCN. Features real-time inventory tracking, automated stock updates, barcode scanning, and detailed sales reports for data-driven decision-making.",
-    url: "https://happyfeet-and-apparel.vercel.app/",
+    url: "https://project01-zeta-puce.vercel.app/",
   },
   {
     name: "Valencia City Public Employment Services Office System",
@@ -17,10 +17,10 @@ export const projects = [
       "A web-based system for tracking and managing construction projects using Laravel, MySQL, JavaScript, jQuery, and DataTables. Provided real-time project monitoring, data visualization, and reporting. Designed with MVC architecture for scalability and maintainability.",
     url: "https://construction-monitoring.jaymarknicolas.com/",
   },
-  // {
-  //   name: "WidgetFlow – Customizable Productivity Dashboard",
-  //   description:
-  //     "A single-page application (SPA) built with Next.js, React, TypeScript, and Tailwind CSS, allowing users to create a personalized workspace with draggable widgets such as a to-do list, Pomodoro timer, and embeddable webpages. Integrated NextAuth for authentication and Prisma with PostgreSQL for data management.",
-  //   url: "https://widget-flow-snowy.vercel.app/",
-  // },
+  {
+    name: "WidgetFlow – Customizable Productivity Dashboard",
+    description:
+      "A single-page application (SPA) built with Next.js, React, TypeScript, and Tailwind CSS, allowing users to create a personalized workspace with draggable widgets such as a to-do list, Pomodoro timer, and embeddable webpages. Integrated NextAuth for authentication and Prisma with PostgreSQL for data management.",
+    url: "https://widgetflow.jaymarknicolas.com/",
+  },
 ];
